2f66320b224f06893c524a318b72fb109358b13e
m-zepeda <mzepeda@xoarmortech.com>
2026-05-12 11:09:03 -0600

    chore: change allowed origins for public ip requests

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    '192.168.1.93',
-    '192.168.1.11',
-    'motivate-slideshow-gorged.ngrok-free.dev',
-  ],
+  allowedDevOrigins: ['192.168.1.50', '181.115.102.180'],
 };
 
 export default nextConfig;
